ba9e835b78ced961f0762ca61132e43292425eaa
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-06-11 22:02:59 -0400

    absolute silliness here - adding dotenv

--- react-app/src/firebaseConfig.ts
+++ react-app/src/firebaseConfig.ts
@@ -8,13 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDfreUUCm8xeI6QW2tXnf1yaVmMRNnWJsU",
-    authDomain: "portfolio-project-f165c.firebaseapp.com",
-    projectId: "portfolio-project-f165c",
-    storageBucket: "portfolio-project-f165c.firebasestorage.app",
-    messagingSenderId: "958477223499",
-    appId: "1:958477223499:web:694b067f0bf8837a322fce",
-    measurementId: "G-NH2RCTYXFZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const root = document.getElementById("root");
 
