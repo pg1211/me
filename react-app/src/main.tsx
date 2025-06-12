816072c5841b78ac7d37fee858a7ea586846168a
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-06-11 21:12:29 -0400

    debugging vite-env, tsconfig, emptying out App.css and remedying build in App.tsx

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,17 +1,11 @@
 // @ts-ignore
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const root = document.getElementById("root");
-
-if (root) {
-    ReactDOM.createRoot(root).render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    );
-} else {
-    throw new Error('Root element not found');
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
